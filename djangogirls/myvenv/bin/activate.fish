c072fda8c86116d51a92621cedd49938c7115a3e
manuelp1601 <mpaaredes@ivytech.edu>
2023-05-11 22:48:17 -0400

    M06

--- djangogirls/myvenv/bin/activate.fish
+++ djangogirls/myvenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/manuelp/Documents/SDEV220/Sdev220Homework/djangogirls/myvenv"
+set -gx VIRTUAL_ENV "/Users/manuelp/Documents/GitHub/Sdev220Homework/djangogirls/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
